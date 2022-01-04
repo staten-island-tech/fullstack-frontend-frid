4183ce9f6dfe7de71cf261cad4bebb55bc9214c8
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2022-01-04 12:28:58 -0500

    1/4/22 12:28 PM

--- frontend/components/Login.vue
+++ frontend/components/Login.vue
@@ -1,28 +1,77 @@
 <template>
-  <div class="login-container">
-    <form>
-    <label for="username">Username:</label>
-    <input class="border py-2 px-3 text-grey-darkest" type="text" id="username" name="username">
-    <label for="password">Password:</label>
-    <input type="text" id="password" name="password">
-    </form>
-    <button class="Login-btn">Log In</button>
-    <h1><a href="#">Forgot Password?</a></h1>
- 
+  <div class="sign-up">
+    <sect class="registration-Box">
+      <div>
+        <input class="username-input" type="text" placeholder="Username" />
+        <input
+          class="
+            h-[5vh]
+            w-[25vw]
+            m-[5vh]
+            mb-[1vh]
+            p-[10px]
+            border-solid border-black border
+          "
+          type="password"
+          placeholder="Password"
+        />
+        <button
+          class="bg-sky-300 w-[10vw] h-[5vh] m-[10vw] mt-[5vh] rounded-2xl"
+        >
+          Log In
+        </button>
+      </div>
+    </sect>
   </div>
 </template>
- 
+
 <script>
-export default {};
+export default {
+  name: "SignUp_Page",
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
- 
+
 <style scoped>
- 
-.login-container {
-  border: solid black 1rem;
-  width: 25%;
+.sign-up {
+  height: 100vh;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
- 
+
+.registration-Box {
+  height: 60vh;
+  width: 30vw;
+  display: flex;
+  justify-content: center;
+  border-style: solid;
+  border-color: black;
+  border-width: 2px;
+}
+
+.email-input {
+  height: 5vh;
+  width: 25vw;
+  border-style: solid;
+  border-color: black;
+  border-width: 1px;
+  margin: 5vh;
+  margin-bottom: 1vh;
+  padding: 10px;
+}
+
+.username-input {
+  height: 5vh;
+  width: 25vw;
+  border-style: solid;
+  border-color: black;
+  border-width: 1px;
+  margin: 5vh;
+  margin-bottom: 1vh;
+  padding: 10px;
+}
 </style>
