6a63fbdb5ab332f3952e5260e8db1c07291a747a
StanleyRabkin <stanley.rabkin23@sitechhs.com>
2022-01-06 12:24:30 -0500

    01/06/2022 12:28

--- frontend/components/Login.vue
+++ frontend/components/Login.vue
@@ -1,20 +1,18 @@
 <template>
-   <div class=""> 
-    <sect class="flex flex-wrap flex-col">
-      <div>
-        <input class="" type="text" placeholder="Username" />
+  <div class="flex items-center justify-center h-[100vh]">
+   <div class="border-2 border-black flex items-center justify-center flex-col p-[4rem]" > 
+        <input class="mb-[1rem] mt-[2rem] border-2 border-black p-[0.5rem] rounded-md" type="text" placeholder="Username" />
         <input
-          class=""
+          class="mb-[1rem] border-2 border-black p-[0.5rem] rounded-md"
           type="password"
           placeholder="Password"
         />
         <button
-          class=""
+          class="mt-[1rem] mb-[2rem] border-2 border-black p-[0.5rem] text-[1.65rem]"
         >
           Log In
         </button>
-      </div>
-    </sect>
+    </div>
   </div>
 </template>
 
