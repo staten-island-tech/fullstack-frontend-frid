8a334014ff030b1e427d9119c7924fe2f265ffc0
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2022-01-15 10:18:00 -0500

    1/15/22 10/17 AM
    working on profile page

--- frontend/components/ProfilePage.vue
+++ frontend/components/ProfilePage.vue
@@ -1,38 +1,119 @@
 <template>
   <div class="bg-sky-400">
-    <div class="flex flex-col items-center justify-center h-[100vh]">
+    <div
+      class="
+        border-2 border-black
+        rounded-[1.5rem]
+        flex
+        self-end
+        items-center
+        justify-center
+        flex-col
+        p-[4rem]
+        h-[12vh]
+        w-[70vw]
+        mx-[2.5vw]
+      "
+    ></div>
+    <div
+      class="
+        border-2 border-black
+        flex flex-row
+        items-center
+        justify-center
+        h-[80vh]
+      "
+    >
+      <div
+        class="
+          border-2 border-slate-400
+          flex flex-col
+          items-center
+          justify-center
+          h-[80vh]
+          w-[30vw]
+        "
+      >
+        <p
+          class="
+            border-2 border-black
+            rounded-[1rem]
+            flex
+            self-end
+            items-center
+            justify-center
+            flex-col
+            p-[1rem]
+            mx-[2.5vw]
+            my-[5vh]
+            w-[15vw]
+          "
+        >
+          All Posts
+        </p>
+        <p
+          class="
+            border-2 border-black
+            rounded-[1rem]
+            flex
+            self-end
+            items-center
+            justify-center
+            flex-col
+            p-[1rem]
+            mx-[2.5vw]
+            mb-[5vh]
+            w-[15vw]
+          "
+        >
+          Followers <br />
+          & <br />
+          Following
+        </p>
+        <p
+          class="
+            border-2 border-black
+            rounded-[1rem]
+            flex
+            self-end
+            items-center
+            justify-center
+            flex-col
+            p-[1rem]
+            mx-[2.5vw]
+            mb-[5vh]
+            w-[15vw]
+          "
+        >
+          User Settings
+        </p>
+      </div>
       <div
         class="
           border-2 border-black
-          rounded-[1.5rem]
-          flex
-          self-end
+          flex flex-col
           items-center
           justify-center
-          flex-col
-          p-[4rem]
-          h-[12vh]
-          w-[70vw]
-          mx-[2.5vw]
-          my-[5vh]
+          h-[80vh]
         "
-      ></div>
-      <div
-        class="
-          border-2 border-black
-          rounded-[1.5rem]
-          flex
-          self-end
-          items-center
-          justify-center
-          flex-col
-          p-[4rem]
-          h-[70vh]
-          w-[70vw]
-          mx-[2.5vw]
-          mb-[5vh]
-        "
-      ></div>
+      >
+        <div
+          class="
+            border-2 border-black
+            rounded-[1.5rem]
+            flex
+            self-end
+            items-center
+            justify-center
+            flex-col
+            p-[4rem]
+            h-[70vh]
+            w-[70vw]
+            mx-[2.5vw]
+            mb-[5vh]
+          "
+        ></div>
+      </div>
     </div>
   </div>
 </template>
