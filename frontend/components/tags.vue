8bb8205c65e80196c918f024eddaf53f253c1942
StanleyRabkin <71650302+StanleyRabkin@users.noreply.github.com>
2022-05-21 22:03:17 -0400

    05/21/22 10:03 PM

---
+++ frontend/components/tags.vue
@@ -0,0 +1,47 @@
+<template></template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      tags: [
+        "Accapella",
+        "Alternative Rock",
+        "Blues",
+        "Classical",
+        "Classic Rock",
+        "Country",
+        "Desi",
+        "Disco",
+        "Drill",
+        "Electronic",
+        "Folk",
+        "Foreign",
+        "Grime",
+        "Grunge",
+        "Hard Rock",
+        "Hip-hop",
+        "Indie",
+        "Instrumental",
+        "Jazz",
+        "Metal",
+        "Melodic Rap",
+        "Mumble Rap",
+        "Musical Theatre",
+        "Nightcore",
+        "Opera",
+        "Pop",
+        "Psychadelic Rock",
+        "Punk Rock",
+        "R&B",
+        "Rap",
+        "Reggae",
+        "Rock",
+        "Soul",
+        "Techno",
+      ],
+    };
+  },
+};
+</script>
